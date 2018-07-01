1c68ea0460a22a2bcf126511daec0cad8f43ebaa
Takuya0620 <b161902@hiroshima-u.ac.jp>
2018-07-01 21:26:50 +0900

    フィルタの適用

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # i-cam
 interactive camera
+
+- コントラスト補正はRGB各要素にシグモイド関数に従うトーンカーブを適用することで実装.
+- 色相, 彩度, 明度はRGB色空間からHSV色空間に変換してからそれぞれの要素をガンマ変換し, その後RGB色空間に逆変換することで実装.
+- ローパスフィルタをFFTを用いて実装した.
+
+ガンマ変換の$\gamma=0$の値はより小さい値で補完することで, 直感的にトラックバーを操作できるようにした.
+
+S字トーンカーブはシグモイド関数で実装できる.
+<http://www.hamanoweb.com/blog/?p=3408>
+
+OpenCVにおける色彩空間の変換
+https://bi.biopapyrus.jp/ia/opencv/color-space.html
+
+OpenCVによるフーリエ変換
+https://algorithm.joho.info/programming/python/opencv-fft-low-pass-filter-py/
--- interactive_camera.py
+++ interactive_camera.py
@@ -2,15 +2,71 @@ import numpy as np
 import cv2
 
 def myfunc(i):
-    pass # do nothing
+    pass #do nothing
 
-cv2.namedWindow('title') # create win with win name
+# calclate sigmoid function
+def sig(i, x):
+    return 1 / (1 + np.exp(- i * x))
 
-cv2.createTrackbar('value', # name of value
-                   'title', # win name
+# change contrast by using sigmoid function
+def changeGrayContrast(i, x):
+    MAX = 1
+    if i != 0:
+        x = x / 255 * MAX
+        x = (sig(i, x - MAX / 2) - sig(i, 0 - MAX / 2)) * (255 / (sig(i, MAX - MAX / 2) - sig(i, 0 - MAX / 2)))
+    return x
+
+# x.shape == (*,*,3)
+def changeContrast(i, x):
+    x[:,:,0] = changeGrayContrast(i, x[:,:,0])
+    x[:,:,1] = changeGrayContrast(i, x[:,:,1])
+    x[:,:,2] = changeGrayContrast(i, x[:,:,2])
+    return x
+    
+def gammaCorrect(r, x):
+    if r != 0:
+        x = 255 * np.power(x / 255, 1 / r)
+    else:
+        x = 255 * np.power(x / 255, 1 / 0.001)
+    return x
+
+def gaussianFilter(r, x):
+    average_square = (r, r)
+    x = cv2.GaussianBlur(x, average_square, 1)
+    return x
+
+cv2.namedWindow('interactive-camera') # create win with win name
+
+cv2.createTrackbar('contrast', # name of value
+                   'interactive-camera', # win name
                    0, # min
+                   9, # max
+                   myfunc) # callback func
+
+cv2.createTrackbar('hue', # name of value
+                   'interactive-camera', # win name
+                   10, # min
                    100, # max
                    myfunc) # callback func
+
+cv2.createTrackbar('saturation', # name of value
+                   'interactive-camera', # win name
+                   10, # min
+                   100, # max
+                   myfunc) # callback func
+
+cv2.createTrackbar('value', # name of value
+                   'interactive-camera', # win name
+                   10, # min
+                   100, # max
+                   myfunc) # callback func
+
+cv2.createTrackbar('gaussian', # name of value
+                   'interactive-camera', # win name
+                   0, # min
+                   99, # max
+                   myfunc) # callback func
+
 
 
 cap = cv2.VideoCapture(0)
@@ -22,14 +78,40 @@ while(True):
 
     ret, frame = cap.read()
     if not ret: continue
-
-
+    
+    a = cv2.getTrackbarPos('contrast',  # get the value
+                       'interactive-camera')  # of the win
+                       
+    h = cv2.getTrackbarPos('hue',  # get the value
+                           'interactive-camera')  # of the win
+    s = cv2.getTrackbarPos('saturation',  # get the value
+                           'interactive-camera')  # of the win
     v = cv2.getTrackbarPos('value',  # get the value
-                           'title')  # of the win
+                       'interactive-camera')  # of the win
+    r = cv2.getTrackbarPos('gaussian',  # get the value
+                       'interactive-camera')  # of the win
+    
+    # change contrast
+    if a != 0:
+        frame = changeContrast(a, frame)
+    
+    # change hue, saturation and value
+    frame_hls = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+    if h != 10:
+        frame_hls[:,:,0] = gammaCorrect(h / 10, frame_hls[:,:,0])
+    if s != 10:
+        frame_hls[:,:,1] = gammaCorrect(s / 10, frame_hls[:,:,1])
+    if v != 10:
+        frame_hls[:,:,2] = gammaCorrect(v / 10, frame_hls[:,:,2])
+    frame = cv2.cvtColor(frame_hls, cv2.COLOR_HSV2RGB)
+    
+    # low pass filter
+    if r != 0 and r % 2 == 1:
+        frame = gaussianFilter(r, frame)
+    elif r != 0 and r % 2 != 1:
+        frame = gaussianFilter(r+1, frame)
 
-    ## do something by using v
-
-    cv2.imshow('title', frame)  # show in the win
+    cv2.imshow('interactive-camera', frame)  # show in the win
 
     k = cv2.waitKey(1)
     if k == ord('q') or k == 27:
